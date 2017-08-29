90050baed6c440b3e438cd807b77d1def3fd2c60
Jaruwat Sukkhamjohn <benzser@gmail.com>
2017-08-29 10:39:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jaruwatsukkhamjohn/liveat500px/MainApplication.java
@@ -0,0 +1,23 @@
+package com.example.jaruwatsukkhamjohn.liveat500px;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+/**
+ * Created by Jaruwat Sukkhamjohn on 28/8/2560.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
